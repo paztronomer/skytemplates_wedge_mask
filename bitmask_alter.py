25a11ce93711373e38b2d1976cc1ce17e576ff94
Francisco Paz-Chinchón <fpazch@illinois.edu>
2018-03-23 15:50:46 -0500

    Code is completed and workign

--- bitmask_alter.py
+++ bitmask_alter.py
@@ -8,6 +8,7 @@ import sys
 import time
 import logging
 import argparse
+import copy
 import numpy as np
 from functools import partial
 import multiprocessing as mp
@@ -68,15 +69,18 @@ def open_fits(fnm, ):
     ''' Open the FITS, read the data and store it on a list
     '''
     tmp = fitsio.FITS(fnm)
-    data = []
+    data, header = [], []
     for extension in tmp:
         ext_tmp = np.copy(extension.read())
         data.append(ext_tmp)
+        header_tmp = copy.deepcopy(extension.read_header())
+        header.append(header_tmp)
+    tmp.close()
     if (len(data) == 0):
         logging.error('No extensions were found on {0}'.format(fnm))
         exit(1)
     elif (len(data) >= 1):
-        return data
+        return data, header
 
 def open_txt(fnm, Nskip):
     ''' Open the text file containing the 2 coordinates per box (bottom-left
@@ -178,6 +182,7 @@ def bit_superpose(box_i,
         logging.warning('The masking of all pixels failed')
     # Unmask the box region to operate on it.
     # NOTE: np.ma.nomask assign zeros instead of recover original data
+    # NOTE: np.ma.getdata returns ndarray
     mask_box[box_i['y1']:box_i['y2'] ,
              box_i['x1']:box_i['x2']] = mask_box[box_i['y1']:box_i['y2'] ,
                                                  box_i['x1']:box_i['x2']].data
@@ -213,10 +218,15 @@ def bit_superpose(box_i,
     return mask_box
 
 def joint_mask(bpm,
+               header_orig,
                box_msk,
+               fname_bpm_orig,
                non_masked_val=0,
                mask_unmasked=True,
-               Nproc=None):
+               Nproc=None,
+               out_dtype='uint16',
+               file_check=False,
+               out_fname=None,):
     ''' Applies the box-defined mask to the original BPM, avoiding replace
     bits values. The flag need to have an entry in BPMDEF.
     The bit comparison is carried out in parallel, one process per box.
@@ -230,6 +240,8 @@ def joint_mask(bpm,
     the flag value to the already masked pixels
     - Nproc: number of processes to be run in parallel for the bit comparison,
     one per box
+    - out_dtype: dtype to be used in the BPMs
+    - file_check: wether to check for existing file
     '''
     # Check the bits
     #
@@ -261,6 +273,11 @@ def joint_mask(bpm,
     #
     bpm_twin = np.copy(bpm)
     # ====Run in parallel====
+
+    # WARNING: test with only 1 mbox
+    logging.warning('TEST WITH ONLY ONE BOX')
+    box_msk = box_msk[0]
+
     # For Python 3+ the map can be applied in a easier way
     kw_super = {
         'bpm' : bpm,
@@ -269,30 +286,101 @@ def joint_mask(bpm,
         'non_masked_val' : non_masked_val,
         'mask_unmasked' : mask_unmasked,
     }
-    t0 = time.time()
-    if (Nproc is None):
-        Nproc = len(box_msk)
-    P1 = mp.Pool(processes=Nproc)
-    # Using the partial() method define an auxiliary function with the fixed
-    # values. Then, in the map the iterating value will be provided
-    part_bit_superpose = partial(bit_superpose, **kw_super)
-    # The map blocks until the result is ready. If want to use apply_async()
-    # or other method, processes could be freed when ends, without wait for
-    # the others
-    mask_i_box = P1.map(part_bit_superpose, box_msk)
-    t1 =  time.time()
-    txt_time = 'Time in comparing bits: {0:.2f} min'.format((t1 - t0) / 60.)
-    logging.info(txt_time)
+    if (box_msk.size == 0):
+        logging.warning('No boxes to process. Exiting')
+        exit(1)
+    elif (box_msk.size == 1):
+        t0 = time.time()
+        mask_i_box = [bit_superpose(box_msk, **kw_super),]
+        t1 =  time.time()
+        txt_time = 'Time in comparing bits: {0:.2f} min'.format((t1 - t0) / 60.)
+        logging.info(txt_time)
+    elif (box_mask.size > 1):
+        t0 = time.time()
+        if (Nproc is None):
+            Nproc = len(box_msk)
+        P1 = mp.Pool(processes=Nproc)
+        # Using the partial() method define an auxiliary function with the fixed
+        # values. Then, in the map the iterating value will be provided
+        part_bit_superpose = partial(bit_superpose, **kw_super)
+        # The map blocks until the result is ready. If want to use apply_async()
+        # or other method, processes could be freed when ends, without wait for
+        # the others
+        mask_i_box = P1.map(part_bit_superpose, box_msk)
+        t1 =  time.time()
+        txt_time = 'Time in comparing bits: {0:.2f} min'.format((t1 - t0) / 60.)
+        logging.info(txt_time)
     # =======================
     # Replace values on the BPM array
     for i_box in mask_i_box:
         bpm_twin[~i_box.mask] = i_box[~i_box.mask]
-
+    # Unmask the array
+    bpm_final = np.ma.getdata(bpm_twin)
+    #
+    # Save into FITS
+    # Use the original keys for CCDNUM and DES_EXT
+    #
+    if (out_fname is None):
+        out_fname = 'bpm_modif_{0}'.format(fname_bpm_orig)
+    #
+    # Check if file already exists
+    if (os.path.exists(out_fname) and file_check):
+        logging.warning('File {0} already exists'.format(out_fname))
+        while True:
+            ask_txt = '\n\n===========================\n'
+            ask_txt += 'File {0} already exists\n'.format(out_fname)
+            ask_txt += 'Do you want to: \n\t-Overwrite? [Y]'
+            ask_txt += '\n\t-Do not modify and exit? [N]'
+            ask_txt += '\n\t-Add result as a new FITS extension? [A]'
+            ask_txt += '\nAnswer [Y/N/A] (default=Y): '
+            choice = raw_input(ask_txt).lower()
+            choice = str.strip(choice)
+            if (choice == 'y' or (choice == '')):
+                os.remove(out_fname)
+                break
+            elif (choice == 'n'):
+                logging.info('Ok, exiting.')
+                exit(0)
+                break
+            elif (choice == 'a'):
+                break
+            else:
+                pass
+    elif (os.path.exists(out_fname) and (not file_check)):
+        logging.info('Overwritting {0}'.format(out_fname))
+        os.remove(out_fname)
+    #
+    # Initialize object
+    fits = fitsio.FITS(out_fname, 'rw')
+    # Write array and header
+    fits.write(bpm_final, dtype=out_dtype)#, header=header_orig)
+    # Modify header
+    comm1 = 'Wedge mask added by F Paz-Chinchon to {0}'.format(fname_bpm_orig)
+    hlist = [
+        {'name' : 'CCDNUM',
+         'value' : header_orig['CCDNUM'],
+         'comment' : 'CCD Number'
+        },
+        {'name' : 'DES_EXT',
+         'value' : header_orig['DES_EXT'],
+         'comment' : 'Extension type',
+        },
+        {'name' : 'DESMKBPM', 'value' : time.ctime(),
+         'comment' : 'bad pixel map created'},
+        {'name' : 'COMMENT',
+         'value' : comm1},
+    ]
+    fits[0].write_keys(hlist)
+    # Write checksum
+    fits[0].write_checksum()
+    # Close FITS
+    fits.close()
+    logging.info('Modified BPM saved as {0}'.format(out_fname))
     #
     # Diagnosis plot
     #
     # Stacked mask
-    if True:
+    if False:
         plt.close('all')
         fig = plt.figure(figsize=(5, 8))
         ax0 = fig.add_subplot(1, 1, 1)
@@ -315,22 +403,25 @@ def joint_mask(bpm,
         plt.colorbar(im)
         plt.show()
         del aux_bpm
+    return True
 
 
 if __name__ == '__main__':
-    desc_text = 'Per-CCD code. Based on BPM, and box coodinates'
-    desc_text += ' (x1, y1, x2, y2) for new masks to be included on the'
-    desc_text += ' initial BPM'
-    abc = argparse.ArgumentParser(description=desc_text)
+    desc_txt = 'Code to add a mask to a BPM FITS file, based on coordinates'
+    desc_txt += ' (given in an input table), and the BPMDEF flag to be assumed'
+    desc_txt += ' by the masked pixels. It works per-CCD, one file at a time.'
+    desc_txt += ' The input table must contain rows as (x1, y1, x2, y2,'
+    desc_txt += ' bpmdef_flag). NOTE: it add the bits to the mask'
+    abc = argparse.ArgumentParser(description=desc_txt)
     h0 = 'Image to be loaded, to check the masking'
     tmp_pca = 'Y4A1_20160801t1215_g_c03_r2930p01_skypca-tmpl.fits'
     abc.add_argument('--image', help=h0, default=tmp_pca)
     h1 = 'BPM FITS file'
     tmp_bpm = 'D_n20170815t0824_c03_r3370p01_bpm.fits'
     abc.add_argument('--bpm', help=h1, default=tmp_bpm)
-    h2 = 'Space-sepatared file with 2 vertices (bottom-left and top-right) per'
-    h2 += ' box, to be be included in the BPM masking. Format should be: x_ini'
-    h2 += ' y_ini x_end y_end bpmdef_flag'
+    h2 = 'Space-separated file with 2 vertices (bottom-left and top-right) per'
+    h2 += ' box and its BPMDEF flag, to be be included in the BPM masking.'
+    h2 += ' Format should be: x_ini y_ini x_end y_end bpmdef_flag'
     tmp_add = 'vert_flag_op3.txt'
     abc.add_argument('--tab', help=h2, default=tmp_add)
     h3 = 'How many lines skip at the top of the space-separated file defining'
@@ -339,17 +430,27 @@ if __name__ == '__main__':
     h4 = 'How many processes to run in parallel for the mask stacking. Default'
     h4 += ' is the number of boxes'
     abc.add_argument('--n1', help=h4, type=int)
+    h5 = 'Value for non-masked pixels. Default: 0'
+    abc.add_argument('--val', help=h5, type=int, default=0)
+    h6 = 'Data type for the output BPM file. Default: uint16 (unsigned integer)'
+    abc.add_argument('--dtype', help=h6, type=str, default='uint16')
+    h7 = 'Flag to check for existing file, avoiding overwrite (prompt is'
+    h7 += ' displayed). Default is to overwrite'
+    abc.add_argument('--check', help=h7, action='store_true')
+    h8 = 'Output filename. Default is bpm_modif_{filename of input BPM}.fits'
+    abc.add_argument('--out', help=h8)
     # Variables
     abc = abc.parse_args()
-
-    aux_pca = open_fits(abc.image)
-    aux_bpm = open_fits(abc.bpm)
+    #
+    # aux_img, header_img = open_fits(abc.image)
+    aux_bpm, header_bpm = open_fits(abc.bpm)
     aux_tab = open_txt(abc.tab, abc.skip)
     # We expect one extension.
     if (len(aux_bpm) != 1):
         logging.error('This code is implemented to update one-extension only')
         exit(1)
     # Call different flags, for different boxes. One flag at a time
-    aux_mask = joint_mask(aux_bpm[0], aux_tab, Nproc=abc.n1)
-
-    # print(bpmdef.keys(), bpmdef.values())
+    aux_mask = joint_mask(aux_bpm[0], header_bpm[0], aux_tab, abc.bpm,
+                          Nproc=abc.n1, non_masked_val=abc.val,
+                          out_dtype=abc.dtype, file_check=abc.check,
+                          out_fname=abc.out)
